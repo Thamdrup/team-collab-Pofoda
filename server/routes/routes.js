7eb5277d56df61a75ae1ce8b959fb6e0cf299337
obaidullah obi <obaidullahahmad3965@gmail.com>
2019-08-16 12:32:57 +0200

    tester

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,17 +3,11 @@ const mysql = require('../config/mysql');
 module.exports = (app) => {
 
     app.get('/', async (req, res, next) => {
-        let db = await mysql.connect();
-        let [categories] = await db.execute('SELECT * FROM categories'); // Nav
-        let [posts] = await db.execute(`
-SELECT * FROM opslag
-ORDER by post_date desc
-`); // Nyeste dato øverst
+       
 
-        db.end();
-        res.render('home', {
-            "categories": categories,
-            "posts": posts,
+        
+        res.render('home.ejs', {
+            "title": "shopping"
         });
     });
 
