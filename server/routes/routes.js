5f8b137992dea4ee6f4d4e78f6607d49bc1c644c
Pofoda <3011904@rts-365.dk>
2019-08-16 11:21:13 +0200

    update

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,16 +4,11 @@ module.exports = (app) => {
 
     app.get('/', async (req, res, next) => {
         let db = await mysql.connect();
-        let [categories] = await db.execute('SELECT * FROM categories'); // Nav
-        let [posts] = await db.execute(`
-SELECT * FROM opslag
-ORDER by post_date desc
-`); // Nyeste dato øverst
+        
 
         db.end();
         res.render('home', {
-            "categories": categories,
-            "posts": posts,
+            
         });
     });
 
