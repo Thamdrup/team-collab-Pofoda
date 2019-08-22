66642e54c56ca35f83349c7d325fff400d6bd473
Thamdrup <43438980+Thamdrup@users.noreply.github.com>
2019-08-22 10:48:48 +0200

    collection select product

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -26,15 +26,13 @@ module.exports = (app) => {
     app.get('/collection/:category_id', async(req,res,next)=>{
         let db = await mysql.connect();
         let [categories] = await db.execute('SELECT * FROM categories');
-        let [products] = await db.execute('SELECT * FROM products');
-        let [choose_category] = await db.execute('SELECT * FROM products INNER JOIN categories ON category_id = fk_category_id WHERE category_id = ?', [req.params.category_id]);
+        let [products] = await db.execute('SELECT * FROM products INNER JOIN categories ON category_id = fk_category_id WHERE category_id = ?', [req.params.category_id]);
         db.end();
 
         res.render('collection.ejs',{
             "title": "Fabion",
             "categories": categories,
             "products": products,
-            "choose_category": choose_category[0],
             pageLink: "collection"
         })
     })
